5d6800e2e64b41a7503ad66275772b21c5573c99
VladULazarev <68382253+VladULazarev@users.noreply.github.com>
2022-01-23 11:04:11 +0300

    Update Validator.php

--- website/core/Validator.php
+++ website/core/Validator.php
@@ -11,7 +11,7 @@ trait Validator
     private static $namePattern = '/^[\w. \-]+$/u';
 
     /** @var pattern for email */
-    private static $emailPattern = '/^[\w.\-]+@[\w\-]+\.[A-Za-z]{2,6}$/';
+    private static $emailPattern = '/^[\w.\-]{2,30}@[\w\-]{2,30}\.[A-Za-z]{2,8}$/';
 
     /** @var pattern for password */
     private static $passwordPattern = '/^[\w\-]{6,16}$/';
